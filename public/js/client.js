9df84056e546193d26b70886e80bf9fe6f314adc
zanardob <borduchi@outlook.com>
2017-05-24 09:57:47 -0300

    More templates, some AJAX

--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,25 @@
 function index() {
-  $.post('/', function(result){
-    $('#wrapper').html(result);
-  });
+    $.get('/', function(result){
+        $('#wrapper').html(result);
+    });
+}
+
+function cart() {
+    $.get('/cart', function(result){
+        $('section').html(result);
+    })
+}
+
+function login() {
+    $.get('/login', function(result){
+        $('section').html(result);
+    })
+}
+
+function service(serviceName) {
+    if(serviceName == 'tosa'){
+        $.get('/service', function(result){
+            $('section').html(result);
+        })
+    }
 }
--- server.js
+++ server.js
@@ -1,64 +1,86 @@
 var express = require('express');
 var mustache = require('mustache');
 var fs = require("fs");
-
 var app = express();
-
 var templates = {
-  header: fs.readFileSync('template/header.html').toString(),
-  nav_admin: fs.readFileSync('template/nav-admin.html').toString(),
-  nav_index: fs.readFileSync('template/nav-index.html').toString(),
-  page: fs.readFileSync('template/page.html').toString(),
-  product_card: fs.readFileSync('template/product-card.html').toString(),
-  section_admin: fs.readFileSync('template/section-admin.html').toString(),
-  section_index: fs.readFileSync('template/section-index.html').toString(),
+    header: fs.readFileSync('template/header.html').toString(),
+    nav_admin: fs.readFileSync('template/nav-admin.html').toString(),
+    nav_index: fs.readFileSync('template/nav-index.html').toString(),
+    page: fs.readFileSync('template/page.html').toString(),
+    product_card: fs.readFileSync('template/product-card.html').toString(),
+    product: fs.readFileSync('template/product.html').toString(),
+    cart: fs.readFileSync('template/cart.html').toString(),
+    login: fs.readFileSync('template/login.html').toString(),
+    service: fs.readFileSync('template/service.html').toString(),
+    section_admin: fs.readFileSync('template/section-admin.html').toString(),
+    section_index: fs.readFileSync('template/section-index.html').toString()
 }
 
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-   console.log("Got a GET request for the homepage");
-   // req.query.<query_string>
-   var html = mustache.render(templates.page, {  header: mustache.render(templates.header, {cart_count: 1}),
-                            nav: mustache.render(templates.nav_index, {cart_count: 1}),
-                            section: mustache.render(templates.section_index, {
-                              products: function() {
-                                var html = "";
-                                for(var prod = 0; prod < 9; prod++)
-                                {
-                                  html += mustache.render(templates.product_card, {product_id: prod, product_img: 'cat128.png', product_name: 'Produto', product_price: 190.00});
-                                }
-                                return html;
-                              }
-                            })
-                          });
-   res.send(html);
+    console.log("GET request: index");
+    var html = mustache.render(templates.page, {
+        header: mustache.render(templates.header, {cart_count: 1}),
+        nav: mustache.render(templates.nav_index, {cart_count: 1}),
+        section: mustache.render(templates.section_index, {
+            products: function() {
+                var html = "";
+                for(var prod = 0; prod < 8; prod++){
+                    html += mustache.render(templates.product_card, {product_id: prod, product_img: 'cat128.png', product_name: 'Produto', product_price: 190.00});
+                }
+                return html;
+            }
+        })
+    });
+    res.send(html);
 })
 
-// This responds a POST request for the homepage
-app.post('/', function (req, res) {
-   console.log("Got a POST request for the homepage");
-   // req.body.<post_string>
-   var html = mustache.render("{{{header}}} {{{nav}}} {{{section}}}", {  header: mustache.render(templates.header, {cart_count: 1}),
-                            nav: mustache.render(templates.nav_index, {cart_count: 1}),
-                            section: mustache.render(templates.section_index, {
-                              products: function() {
-                                var html = "";
-                                for(var prod = 0; prod < 9; prod++)
-                                {
-                                  html += mustache.render(templates.product_card, {product_id: prod, product_img: 'cat128.png', product_name: 'Produto', product_price: 190.00});
-                                }
-                                return html;
-                              }
-                            })
-                          });
-   res.send(html);
+/*app.post('/', function (req, res) {
+    console.log("POST request: index");
+    var html = mustache.render(templates.page, {
+        header: mustache.render(templates.header, {cart_count: 1}),
+        nav: mustache.render(templates.nav_index, {cart_count: 1}),
+        section: mustache.render(templates.section_index, {
+            products: function() {
+                var html = "";
+                for(var prod = 0; prod < 8; prod++){
+                    html += mustache.render(templates.product_card, {product_id: prod, product_img: 'cat128.png', product_name: 'Produto', product_price: 190.00});
+                }
+                return html;
+            }
+        })
+    });
+    res.send(html);
+})*/
+
+app.get('/product', function (req, res) {
+    console.log("GET request: product");
+    var html = mustache.render(templates.product);
+    res.send(html);
+})
+
+app.get('/cart', function (req, res) {
+    console.log("GET request: cart");
+    var html = mustache.render(templates.cart);
+    res.send(html);
+})
+
+app.get('/login', function (req, res) {
+    console.log("GET request: login");
+    var html = mustache.render(templates.login);
+    res.send(html);
+})
+
+app.get('/service', function (req, res) {
+    console.log("GET request: service");
+    var html = mustache.render(templates.service);
+    res.send(html);
 })
 
 var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
 
-   var host = server.address().address
-   var port = server.address().port
-
-   console.log("Example app listening at http://%s:%s", host, port)
+    console.log("Petzzaria Pet Shop listening at http://%s:%s", host, port);
 })
